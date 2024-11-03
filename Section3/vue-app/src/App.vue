db18bbd6009fdf7d9bd00bebaa1e2932c7d53a1d
Yutong Li (Elton) <eltonyt.li@gmail.com>
2024-11-03 14:54:02 -0600

    Finished section 3

---
+++ Section3/vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <header-component></header-component>
+  <body-component></body-component>
+  <footer-component></footer-component>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue"
+import AppFooter from "./components/AppFooter.vue"
+import AppBody from "./components/AppBody.vue"
+export default {
+  name: "App",
+  components: {
+    HeaderComponent: AppHeader,
+    FooterComponent: AppFooter,
+    BodyComponent: AppBody,
+  }
+}
+</script>
+
+<style>
+
+</style>
